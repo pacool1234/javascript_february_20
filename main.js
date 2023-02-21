d69c8553ee39827a06f0d57c2927a3adabc3df33
pacool1234 <niederleytnerenciso@gmail.com>
2023-02-21 12:33:18 +0100

    Ejercicios finalizados

--- main.js
+++ main.js
@@ -1,143 +1,236 @@
-// First part - Variables
-//alert('JavaScript working properly')
-let WorthlessVariable
-let boolean1 = true
-let boolean2 = false
-const PI = 3.14259
-const TAU = 2 * PI
-const myName = 'Paco'
-const myFavNumber = 7
-let boolean3 = true
-console.log(myName.length)
-console.log(typeof myFavNumber)
-console.log('My name is ' + myName + ' and my favourite number is ' + myFavNumber)
-console.log('I\'ll be and ace in JavaScript by the end of the Bootcamp'.toUpperCase())
-console.log('Hi I\'m an ace'.substring(0,5))
-myFavNumberAsString = myFavNumber.toString()
-console.log(typeof myFavNumberAsString)
-const randomVar = 'mango'
-const randomSentence = `${randomVar} is better than ${randomVar}`
-console.log(randomSentence)
-console.log(PI.toFixed(2))
+// 1 - Booleans
+const boolean1 = true
+const boolean2 = false
 
-// Second part - Arrays
-const emptyArray = []
-const numberArray = [0,1,2,3,4,5,6,7,8,9]
-const numberArray2 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-const evenNumberArray = [0,2,4,6,8]
-const negativeNumberArray = [0,-1,-2,-3,-4,-5,-6,-7,-8,-9]
-const helloWorld = ['Hello', 'World']
-const saveEverything = ['hello', 'what', 23, 42.33, 'so']
-const nestedArray = [[756, 'name'], [225, 'surname'], [298, 'address']]
-console.log(saveEverything.length)
-const newElem = false
-saveEverything.push(newElem)
-console.log(saveEverything)
-console.log(typeof saveEverything[1])
-saveEverything.push('Euralius')
+let booleanAnd = boolean1 && boolean2
+let booleanOr = boolean1 || boolean2
+let booleanNot = !boolean1
+let booleanMix0 = (boolean1 || boolean2) && (boolean1 ||(!boolean1 && !boolean2))
 
-// Third part - Objects
-const Car = {
-    make: 'Bugatti',
-    model: 'Chiron',
-    plateNumber: 'SEX6969'
+// 2 - Operators
+let valueDivision = (17 / 3).toFixed(2)
+let valueRemainder = 17 % 7
+
+// 3 - Programming Logic
+const A = 9
+const B ='9'
+if (A == B) console.log('They are equivalent')
+//if (A === B) console.log('They are equivalent')
+
+let backpack = ['book', 'notebook', 'laptop']
+if (backpack.length > 10) {
+    console.log('I can\'t bear so much stuff.')
+} else if (2 <= backpack.length <= 10) {
+    console.log('I am doing great with my backpack')
+} else {
+    console.log('I think I don\'t need a backpack')
 }
 
-const House = {
-    zipCode: 'TEX666',
-    street: 'False St.',
-    number: 17,
-    floor: 3
+let countToTen = 0
+while (countToTen < 10){
+    countToTen ++
+}
+console.log(`countToTen is now ${countToTen}`)
+
+let dayOff = false
+dayOff === true ? console.log('I don\'t work today') : console.log('I work today')
+
+let loopArray = []
+for (let i = 4; i < 19; i++) {
+    loopArray.push(i)
+}
+console.log(loopArray)
+
+let result = 0
+for (num of loopArray) {
+    result += num
+}
+console.log(result)
+
+const myArray = ['Learning', 'loops', 'with', 'Sofia']
+for (word of myArray) {
+    console.log(word)
+}
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i])
 }
 
-const FullStackDeveloper = {
-    languageArray: ['HTML', 'CSS', 'JavaScript'],
-    projectArray: ['NASA\'s webpage', 'I Love You virus', 'Y2K bug']
-}
-
-const Dog = {
-    name: 'Wolfie',
-    breed: 'German Shepperd',
-    color: 'Brown',
-    age: 4
-}
-
-const News = {
-    headline: 'Complaining about government declared illegal.',
-    body: 'Following mass protests due to social unrest, government has declared...'
-}
-
-const Person = {
-    name: 'Brad',
-    surname: 'Pitt',
-    age: 51
-}
-
-console.log(Person.name)
-console.log(FullStackDeveloper.languageArray[2])
-
-const Laptop = {
-    brand: 'Asus'
-}
-
-console.log(Laptop.brand)
-console.log(Laptop['brand'])
-
-const Concert = {
-    bands: ['The Who', 'Dire Straits', 'Led Zeppelin']
-}
-
-console.log(Concert.bands)
-
-const Led = {
-    lamp1: 'red',
-    lamp2: 'green',
-    lamp3: 'blue'
-}
-
-const RGB = [Led.lamp1, Led.lamp2, Led.lamp3]
-console.log(RGB)
-
-const O_Error = {
-    code: '011010100101101011110001101000' 
-}
-
-console.log(O_Error.code)
-
-const Group = {
-    members: ['Roger Daltrey', 'Pete Townsend', 'John Enstwistle', 'Keith Moon']
-}
-
-const members = Group.members
-console.log(members[3])
-
-const Printer = {
-    ink: {
-        red: 0.99,
-        green: 0.54,
-        blue: 0.78
+for (let i = 0; i < 20; i++) {
+    if (i % 3 === 0) {
+        console.log('Potato')
     }
 }
 
-const inkLevel = Printer.ink
-console.log(inkLevel)
+// Extras
+let gente = [
+    {
+      nombre: "Jamiro",
+      edad: 45,
+    },
+    {
+      nombre: "Juan",
+      edad: 35,
+    },
+    {
+      nombre: "Paco",
+      edad: 34,
+    },
+    {
+      nombre: "Pepe",
+      edad: 14,
+    },
+    {
+      nombre: "Pilar",
+      edad: 24,
+    },
+    {
+      nombre: "Laura",
+      edad: 24,
+    },
+    {
+      nombre: "Jenny",
+      edad: 10,
+    }
+  ]
 
-const Mobile = {
-    specs: 'whatever'
+let fourLetterNames = []
+for (obj of gente) {
+    if (obj.nombre.length === 4) {
+        fourLetterNames.push(obj)
+    }
 }
+console.log(fourLetterNames)
 
-const specs = Mobile['specs']
-console.log(specs)
+let jayFortyNames = []
+for (obj of gente) {
+    if (obj.nombre.startsWith('J') && obj.edad < 40) {
+        jayFortyNames.push(obj)
+    }
+}
+console.log(jayFortyNames)
 
-Laptop.brand = 'MSI'
-console.log(Laptop.brand)
+//====================================
+let ordenador = {
+    marca: 'La pava',
+    tipo: 'portatil',
+    perifericos: ['mouse', 'touchPad'],
+    almacenamiento: {
+        discos: ['SATA', 'HDD'],
+        maestro: 0
+    }
+}
+// TEST 1
+if (typeof ordenador === "object") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 2
+  if (ordenador.marca === "La pava") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 3
+  if (ordenador.tipo === "portatil") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 4
+  if (ordenador.perifericos[1] === "touchPad") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 5
+  if (ordenador["almacenamiento"]["discos"][1] === "HDD") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 6
+  if (ordenador.almacenamiento.maestro === 0) {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
 
-Concert.bands.push('Guns N\' Roses')
-console.log(Concert.bands)
+// ============================
 
-Concert.date = '2023-02-20'
-Concert.date = new Date()
-console.log(Concert.date)
+let arr = [{name: 'pepito', age: 25}, 
+           {name: 'pepito', age: 23},
+           {name: 'Johnny', age: 19}]
 
-Group.members.shift()
-console.log(Group.members.length)
+// TEST 1
+if (typeof arr === "object" && arr.length >= 0) {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 2
+  if (typeof arr[0] === "object") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 3
+  if (typeof arr[1] === "object") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 4
+  if (typeof arr[2] === "object") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 5
+  if (arr.length === 3) {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 6
+  if (arr[0].name === arr[1].name) {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+
+
+  // TEST 6
+  if (arr[1].name === "pepito") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+
+
+  // TEST 7
+  if (arr[1].age > arr[2].age) {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 8
+  if (arr[0].age === 25) {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+  // TEST 9
+  if (typeof arr[2].name === "string") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
+
+
+  // TEST 10
+  if (arr[2].name[0] === "J") {
+    console.log("apruebas");
+  } else {
+    console.log("suspendes");
+  }
